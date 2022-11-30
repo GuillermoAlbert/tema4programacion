15a73ad41a4bbe6ac9a7e3931e1c14a174e1d01e
GUILLERMO ALBERT GARCIA <guialbgar2@alu.edu.gva.es>
2022-11-30 18:20:16 +0100

    Añadida primera versión del código

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int absoluto(int valor)
+        {
+            if (valor =< 0)
+                valor = valor + valor;
+            return valor;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(textBox1.Text);
+            absoluto(numero);
+            MessageBox.Show(numero.ToString());
+        }
+    }
+}
