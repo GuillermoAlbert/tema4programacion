256e184547559f11b125f8d31f4808ecbf89bebe
GUILLERMO ALBERT GARCIA <guialbgar2@alu.edu.gva.es>
2022-12-01 10:19:13 +0100

    Código inicial añadido

--- Ejercicio 5/Form1.cs
+++ Ejercicio 5/Form1.cs
@@ -16,5 +16,26 @@ namespace Ejercicio_5
         {
             InitializeComponent();
         }
+        bool mayor(double n1, double n2)
+        {
+            if (n1 > n2)
+                return true;
+            else
+                return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(textBox1.Text);
+            double num2 = double.Parse(textBox2.Text);
+            bool res;
+            res = mayor(num1, num2);
+            if (res == true)
+                MessageBox.Show("El número " + num1.ToString() + " > " + num2.ToString());
+            else
+                MessageBox.Show("El número " + num1.ToString() + " < " + num2.ToString());
+
+
+        }
     }
 }
