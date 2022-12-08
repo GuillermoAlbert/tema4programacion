97f8375bdea11071416ae4e8e74336cb436e2674
GAG22 <guillermo.albert@outlook.com>
2022-12-08 12:27:32 +0100

    Agregado opción de continuar o terminar al final del cálculo en el ejercicio 8.

--- Ejercicio 8/Program.cs
+++ Ejercicio 8/Program.cs
@@ -25,25 +25,33 @@ namespace Ejercicio_8
                 double valormedio = (n1 + n2 + n3) / 3;
                 return valormedio;
             }
-            
-            Console.WriteLine("Ingrese la nota del primer examen:");
-            double nota1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese la nota del segundo examen:");
-            double nota2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese la nota del tercer examen:");
-            double nota3 = double.Parse(Console.ReadLine());
-
-            bool valido = validacion(nota1, nota2, nota3);
-            if (valido == false)
-                Console.WriteLine("Formato de notas erróneo");
-            else
+            bool continuar = true;
+            while (continuar == true)
             {
-                Console.WriteLine("Notas introducidas correctamente");
-                Console.WriteLine("Calculando media");
-                double mediaAlumno = media(nota1, nota2, nota3);
-                Console.WriteLine("La nota media del alumno es: " + mediaAlumno);
-                Console.ReadLine();
-            }
+                Console.WriteLine("Ingrese la nota del primer examen:");
+                double nota1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la nota del segundo examen:");
+                double nota2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la nota del tercer examen:");
+                double nota3 = double.Parse(Console.ReadLine());
+
+                bool valido = validacion(nota1, nota2, nota3);
+                if (valido == false)
+                    Console.WriteLine("Formato de notas erróneo");
+                else
+                {
+                    Console.WriteLine("Notas introducidas correctamente");
+                    Console.WriteLine("Calculando media");
+                    double mediaAlumno = media(nota1, nota2, nota3);
+                    Console.WriteLine("La nota media del alumno es: " + mediaAlumno);
+                    Console.WriteLine("¿Desea continuar ingresando notas? (S/N)");
+                    string respuesta = Console.ReadLine();
+                    if (respuesta.ToLower() != "s")
+                    {
+                        continuar = false;
+                    }
+                }
+            } 
         }
     }
 }
