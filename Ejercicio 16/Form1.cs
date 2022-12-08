e37c734a8de210b69e9a2c73dee6d45f9e178b8c
GAG22 <guillermo.albert@outlook.com>
2022-12-08 19:36:30 +0100

    Primera versión del código para calcular el factorial (no funciona)

--- Ejercicio 16/Form1.cs
+++ Ejercicio 16/Form1.cs
@@ -16,5 +16,21 @@ namespace Ejercicio_16
         {
             InitializeComponent();
         }
+
+        int factorial(int n)
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                n = n + (n * i);
+            }
+            return n;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(textBox1.Text);
+            int res = factorial(num);
+            MessageBox.Show("El factorial de " + num.ToString() + " es " + res.ToString());
+        }
     }
 }
